98bb96f8c46e516bdc177d44ca2b132eb864a91a
timshaww <time4shaw@gmail.com>
2025-02-04 12:25:35 -0500

    lint 3

--- app/api/visit/route.ts
+++ app/api/visit/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
